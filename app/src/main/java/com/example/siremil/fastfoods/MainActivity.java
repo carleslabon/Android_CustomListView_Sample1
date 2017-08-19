7b0ad68ac5cee68e827dc8aa824ced5be7de4fe4
carleslabon <ecarloniel@yahoo.com>
2017-08-20 03:32:41 +0800

    Revision_I

--- app/src/main/java/com/example/siremil/fastfoods/MainActivity.java
+++ app/src/main/java/com/example/siremil/fastfoods/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.siremil.fastfoods;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Hello World!
+        String[] fastFoods = {
+                "Jollibee",
+                "McDonalds",
+                "KFC",
+                "Chowking",
+                "Wendys"
+        };
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fastFoods);
+        ListView fastFoodListView = (ListView) findViewById(R.id.fastFoodListView);
+        fastFoodListView.setAdapter(adapter);
+
+        fastFoodListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        String fastFood = String.valueOf(adapterView.getItemAtPosition(i));
+                        Toast.makeText(MainActivity.this, fastFood, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
 
     }
+
 }
